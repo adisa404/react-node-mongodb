ff7cef7400e4aceb65130a2773caba2fbdab707c
Adisa <adisa.mrvoljak@live.com>
2019-06-20 12:01:39 +0200

    specify children - example

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class FirstComponent extends Component {
-    render() { 
-        return <h1>Hello World</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </div>
+    );
+  }
 }
- 
+
 export default FirstComponent;
