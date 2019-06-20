43f1e97dbc1789359b8b4bf10b1e0eb839bf1546
Adisa <adisa.mrvoljak@live.com>
2019-06-21 00:25:28 +0200

    add conditional css classes

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
 
 class FirstComponent extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
   render() {
+    let tagsLi = this.state.tags.map(tag => <li key={tag}>${tag}</li>);
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>{tagsLi}</ul>
+      </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
