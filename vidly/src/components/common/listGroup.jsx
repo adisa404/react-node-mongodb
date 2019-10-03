41a8c6e456c19cec95546ef2928856cc9fc019ec
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-10-03 15:50:18 +0200

    list group component - pass clicked genre item from list group up to parent

--- vidly/src/components/common/listGroup.jsx
+++ vidly/src/components/common/listGroup.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 const ListGroup = props => {
-  const { items, textProperty, valueProperty } = props;
+  const { items, textProperty, valueProperty, onItemSelect } = props;
   console.log(items);
   return (
     <ul className='list-goup'>
       {items.map(item => (
-        <li key={item[valueProperty]} className='list-group-item'>
+        <li
+          key={item[valueProperty]}
+          onClick={() => onItemSelect(item)}
+          className='list-group-item'
+        >
           {item[textProperty]}
         </li>
       ))}
