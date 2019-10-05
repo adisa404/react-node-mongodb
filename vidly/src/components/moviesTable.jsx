ab8626dc70ff06257e1626eb4fe20be450b97ebe
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-10-06 01:47:18 +0200

    refactoring - extract <tbody> to a component

--- vidly/src/components/moviesTable.jsx
+++ vidly/src/components/moviesTable.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
-import Like from './common/like';
 import TableHeader from './common/tableHeader';
+import TableBody from './common/tableBody';
+import Like from './common/like';
+
 class MoviesTable extends Component {
   columns = [
     { path: 'title', label: 'Title' },
     { path: 'genre.name', label: 'Genre' },
     { path: 'numberInStock', label: 'Stock' },
     { path: 'dailyRentalRate', label: 'Rate' },
-    { key: 'like' }, //like
-    { key: 'delete' } //delete button
+    {
+      key: 'like',
+      content: movie => (
+        <Like onLike={() => this.props.onLike(movie)} liked={movie.liked} />
+      )
+    }, //like
+    {
+      key: 'delete',
+      content: movie => (
+        <button
+          className='btn btn-danger btn-sm'
+          onClick={() => this.props.onDelete(movie)}
+        >
+          Delete
+        </button>
+      )
+    } //delete button
   ];
   render() {
     const { movies, onLike, onDelete, onSort, sortOption } = this.props;
@@ -19,27 +36,12 @@ class MoviesTable extends Component {
           sortOption={sortOption}
           onSort={onSort}
         />
-        <tbody>
-          {movies.map(movie => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <Like onLike={() => onLike(movie)} liked={movie.liked} />
-              </td>
-              <td>
-                <button
-                  className='btn btn-danger btn-sm'
-                  onClick={() => onDelete(movie)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <TableBody
+          data={movies}
+          onLike={onLike}
+          onDelete={onDelete}
+          columns={this.columns}
+        />
       </table>
     );
   }
