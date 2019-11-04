b0d09644bdd300000ceee848b1c4d23da496398a
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2019-11-05 00:43:51 +0100

    deployment - testing stuff

--- api-node/models/genre.js
+++ api-node/models/genre.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const genreSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: true, //
     minlength: 5,
     maxlength: 50
   }
@@ -14,12 +14,15 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = {
-    name: Joi.string().min(5).max(50).required()
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
   };
 
   return Joi.validate(genre, schema);
 }
 
 exports.genreSchema = genreSchema;
-exports.Genre = Genre; 
+exports.Genre = Genre;
 exports.validate = validateGenre;
